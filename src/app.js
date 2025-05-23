9d8e0827b21bf4dce00375da1cc8d8b39296384e
Charlie <charlie.programming@proton.me>
2025-05-23 10:35:00 -0400

    Rudimentary censor
    doesn’t edit message, just reprimands sender - also added an unused censor function that can censor a string

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
+var swearwords = ["shit", "fuck", "damn", "testcensor"]; // sorry for the obscene code (:
+
+function censor(string) { // unused, but can at some point be used to censor strings
+    var newString = string;
+    for (let word in swearwords) {
+        console.log(swearwords[word])
+        newString = string.replaceAll(swearwords[word], '{censored}');
+    }
+    return newString;
+}
+
+// Listens to incoming messages that contain swearwords
+for (let word in swearwords) {
+  app.message(word, async ({ message, say }) => {
+    // say() sends a message to the channel where the event was triggered
+    await say(`Hi <@${message.user}>, could you please not swear in the chat? Thank you!`);
+  });
+}
+
 (async () => {
   // Start your app
   await app.start(process.env.PORT || 3000);
