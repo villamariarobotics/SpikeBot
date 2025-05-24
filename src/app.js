e9b57e2b2b1f61d929e0b77ebf80cd1311765709
Charlie <charlie.programming@proton.me>
2025-05-24 18:09:00 -0400

    Moved functions to separate files

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+import * as Censor from "./censor.js";
+import * as Msgfn from "./message_functions.js"
+
 const { App } = require('@slack/bolt');
 
 // Initializes your app with your bot token and signing secret
@@ -6,94 +9,19 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
-/* some functions to find conversations and messages */
-
-let conservationID;
-// Find conversation ID using the conversations.list method
-async function findConversation(name) {
-  try {
-    // Call the conversations.list method using the built-in WebClient
-    const result = await app.client.conversations.list({
-      // The token you used to initialize your app
-      token: process.env.SLACK_BOT_TOKEN
-    });
-
-    for (const channel of result.channels) {
-      if (channel.name === name) {
-        conversationId = channel.id;
-
-        // Print result
-        console.log("Found conversation ID: " + conversationId);
-        // Break from for loop
-        break;
-      }
-    }
-  }
-  catch (error) {
-    console.error(error);
-  }
-}
-findConversation("bot-testing-public")
-
-// ID of channel you watch to fetch the history for
-let channelId = conservationID;
-
-// Store message
-let message;
-
-// find last message
-async function fetchMessage(id) {
-  try {
-    // Call the conversations.history method using the built-in WebClient
-    const result = await app.client.conversations.history({
-      // The token you used to initialize your app
-      token: process.env.SLACK_BOT_TOKEN,
-      channel: id,
-      // Limit results
-      inclusive: true,
-      limit: 1
-    });
-    // There should only be one result (stored in the zeroth index)
-    message = result.messages[0];
-    // Print message text
-    console.log(message.text);
-    return message.text
-  }
-  catch (error) {
-    console.error(error);
-  }
-}
-
-
-// censor function
-
-var swearwords = ["shit", "fuck", "damn", "testcensor"]; // sorry for the obscene code (:
-
-function censor(string) { // unused, but can at some point be used to censor strings
-    var newString = string;
-    for (let word in swearwords) {
-        console.log(swearwords[word])
-        newString = string.replaceAll(swearwords[word], '{censored}');
-    }
-    return newString;
-}
-
-
-
-//actual actions
 
 // Listens to incoming messages that contain swearwords
-for (let word in swearwords) {
+for (let word in Censor.swearwords) {
   app.message(word, async ({ message, say }) => {
     // say() sends a message to the channel where the event was triggered
     await say(`Hi <@${message.user}>, could you please not swear in the chat? Thank you!`);
   });
 }
 
-// I'm testing if I can receive a message: (using fetchMessage) (this only workks in bot-testing-public)
+// I'm testing if I can receive a message: (using fetchMessage) (this only works in bot-testing-public)
 app.message("testword", async ({ message, say }) => {
     // say() sends a message to the channel where the event was triggered
-    await say(`Hi <@${message.user}>, this message has been recieved! Is this your message: `+fetchMessage(channelId)+'?');
+    await say(`Hi <@${message.user}>, this message has been recieved! Is this your message: `+Msgfn.fetchMessage(Msgfn.bot_testing_public)+'?');
   });
 
 // app start
