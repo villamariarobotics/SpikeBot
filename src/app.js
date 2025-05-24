43efa1da30383942d1b737d7c0d07eaaab48d9dd
Charlie <charlie.programming@proton.me>
2025-05-24 09:03:29 -0400

    Hi, I added a couple functions to find channel ids and to find the latest message in a channel. I also added a function that, when you say "testword" in bot-testing-public, it should (if all the tokens are set correctly) give a reply.

--- src/app.js
+++ src/app.js
@@ -6,6 +6,67 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
+/* some functions to find conversations and messages */
+
+let conservationID;
+// Find conversation ID using the conversations.list method
+async function findConversation(name) {
+  try {
+    // Call the conversations.list method using the built-in WebClient
+    const result = await app.client.conversations.list({
+      // The token you used to initialize your app
+      token: process.env.SLACK_BOT_TOKEN
+    });
+
+    for (const channel of result.channels) {
+      if (channel.name === name) {
+        conversationId = channel.id;
+
+        // Print result
+        console.log("Found conversation ID: " + conversationId);
+        // Break from for loop
+        break;
+      }
+    }
+  }
+  catch (error) {
+    console.error(error);
+  }
+}
+findConversation("bot-testing-public")
+
+// ID of channel you watch to fetch the history for
+let channelId = conservationID;
+
+// Store message
+let message;
+
+// find last message
+async function fetchMessage(id) {
+  try {
+    // Call the conversations.history method using the built-in WebClient
+    const result = await app.client.conversations.history({
+      // The token you used to initialize your app
+      token: process.env.SLACK_BOT_TOKEN,
+      channel: id,
+      // Limit results
+      inclusive: true,
+      limit: 1
+    });
+    // There should only be one result (stored in the zeroth index)
+    message = result.messages[0];
+    // Print message text
+    console.log(message.text);
+    return message.text
+  }
+  catch (error) {
+    console.error(error);
+  }
+}
+
+
+// censor function
+
 var swearwords = ["shit", "fuck", "damn", "testcensor"]; // sorry for the obscene code (:
 
 function censor(string) { // unused, but can at some point be used to censor strings
@@ -17,6 +78,10 @@ function censor(string) { // unused, but can at some point be used to censor str
     return newString;
 }
 
+
+
+//actual actions
+
 // Listens to incoming messages that contain swearwords
 for (let word in swearwords) {
   app.message(word, async ({ message, say }) => {
@@ -24,6 +89,14 @@ for (let word in swearwords) {
     await say(`Hi <@${message.user}>, could you please not swear in the chat? Thank you!`);
   });
 }
+
+// I'm testing if I can receive a message: (using fetchMessage) (this only workks in bot-testing-public)
+app.message("testword", async ({ message, say }) => {
+    // say() sends a message to the channel where the event was triggered
+    await say(`Hi <@${message.user}>, this message has been recieved! Is this your message: `+fetchMessage(channelId)+'?');
+  });
+
+// app start
 
 (async () => {
   // Start your app
